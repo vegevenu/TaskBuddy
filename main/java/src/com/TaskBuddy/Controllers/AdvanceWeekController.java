52f468260c71cceff076dc63a313a97424841cc5
VegeVenu <vege.venumadhav@gmail.com>
2017-07-24 23:20:21 -0500

    Controller class to call advance_week stored procedure

---
+++ main/java/src/com/TaskBuddy/Controllers/AdvanceWeekController.java
@@ -0,0 +1,40 @@
+package com.TaskBuddy.Controllers;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.TaskBuddy.db.ConnectionManager;
+
+/**
+ * @author Siddhardha
+ *
+ * Controller class to call advance_week stored procedure
+ *
+ */
+@XmlRootElement
+public class AdvanceWeekController {
+	
+	private static Connection conn = ConnectionManager.getInstance().getConnection();
+	
+	private static String SQL = "{CALL advance_week()}";
+	
+	private AdvanceWeekController() {
+	}
+	
+	public static boolean advanceWeek() throws SQLException {
+		
+		String sql = SQL;
+		
+		try (
+				CallableStatement cstmt = conn.prepareCall (sql);
+			){
+			
+			cstmt.execute();
+			
+			return true;
+		}
+	}
+}
---
+++ main/java/src/com/TaskBuddy/Controllers/ResetWeekController.java
@@ -0,0 +1,42 @@
+package com.TaskBuddy.Controllers;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.TaskBuddy.db.ConnectionManager;
+
+
+/**
+ * @author Sai
+ *
+ * Controller class to call reset_week stored procedure
+ *
+ */
+
+@XmlRootElement
+public class ResetWeekController {
+	private static Connection conn = ConnectionManager.getInstance().getConnection();
+	
+	private static String SQL = "{CALL reset_week()}";
+	
+	private ResetWeekController() {
+	}
+	
+	public static boolean resetWeek() throws SQLException {
+		
+		String sql = SQL;
+		
+		try (
+				CallableStatement cstmt = conn.prepareCall (sql);
+			){
+			
+			cstmt.execute();
+			
+			return true;
+		}
+	}
+
+}
